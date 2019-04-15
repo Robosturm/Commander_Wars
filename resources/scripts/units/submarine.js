657fef4cdea5f9c257b5800f1ba7e308edcd2465
Robosturm <fake>
2019-04-15 18:45:09 +0200

    sprite update

--- resources/scripts/units/submarine.js
+++ resources/scripts/units/submarine.js
@@ -60,8 +60,8 @@ var Constructor = function()
             var unit = action.getTargetUnit();
             var animation = GameAnimationFactory.createWalkingAnimation(unit, action);
             var unitID = unit.getUnitID().toLowerCase();
-            animation.loadSprite(unitID + "+walk+mask", true, 1.25);
-            animation.loadSprite(unitID + "+walk", false, 1.25);
+            animation.loadSprite(unitID + "+walk+mask", true, 1);
+            animation.loadSprite(unitID + "+walk", false, 1);
             animation.setSound("moveship.wav", -2);
             return animation;
         };
