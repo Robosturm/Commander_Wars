7e95cd2092a7522d808327c7edbb8d049dc697b9
Robosturm <fake>
2019-09-03 16:27:53 +0200

    aw like unloading mod

---
+++ mods/aw_unloading/scripts/general/unit.js
@@ -0,0 +1,9 @@
+UNIT.transporterRefilling = function (unit)
+{
+    // carrier refilling and unmoving is done here
+    for (var i = 0; i < unit.getLoadedUnitCount(); i++)
+    {
+        var transportUnit = unit.getLoadedUnit(i);
+        transportUnit.refill();
+    }
+};
