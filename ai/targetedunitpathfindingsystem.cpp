d5b8d4088857ee55a9f736803bab95d0d0b39e7e
Robosturm <commanderwars@gmx.de>
2019-12-27 11:02:55 +0100

    #90

--- ai/targetedunitpathfindingsystem.cpp
+++ ai/targetedunitpathfindingsystem.cpp
@@ -92,7 +92,8 @@ bool TargetedUnitPathFindingSystem::finished(qint32 x, qint32 y, qint32 movement
     {
         m_FinishNodes.append(std::tuple<qint32, qint32, qint32, float>(x, y, movementCosts, m_Targets[index].z()));
     }
-    if (GameMap::getInstance()->getTerrain(x, y)->getUnit() == nullptr)
+    if ((GameMap::getInstance()->getTerrain(x, y)->getUnit() == nullptr) ||
+        m_pUnit->getIgnoreUnitCollision())
     {
         return (index >= 0);
     }
