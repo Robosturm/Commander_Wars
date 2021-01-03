7a0ae62837d9a7fb92528931e11a94a5e772771d
Robosturm <commanderwars@gmx.de>
2021-01-03 19:22:26 +0100

    #378

--- mods/awdc_unit/scripts/units/flare.js
+++ mods/awdc_unit/scripts/units/flare.js
@@ -2,7 +2,7 @@ FLARE.init = function(unit)
 {
     unit.setAmmo1(3);
     unit.setMaxAmmo1(3);
-    unit.setWeapon1ID("");
+    unit.setWeapon1ID("WEAPON_FLARE");
 
     unit.setAmmo2(-1);
     unit.setMaxAmmo2(-1);
