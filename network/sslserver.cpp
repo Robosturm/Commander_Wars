e5f1040c790bd61150aa101413fe7479135ead4b
Robosturm <commanderwars@gmx.de>
2022-06-04 17:06:00 +0200

    removed ssl error print

--- network/sslserver.cpp
+++ network/sslserver.cpp
@@ -13,17 +13,8 @@ SslServer::SslServer(QObject *parent)
 void SslServer::incomingConnection(qintptr socketDescriptor)
 {
     QSslSocket *sslSocket = new QSslSocket(this);
-    connect(sslSocket, QSslSocket::sslErrors, this, SslServer::sslErrors, Qt::QueuedConnection);
     sslSocket->setSocketDescriptor(socketDescriptor);
     sslSocket->setPeerVerifyMode(QSslSocket::PeerVerifyMode::VerifyNone);
     sslSocket->setPrivateKey(m_key);
     addPendingConnection(sslSocket);
-}
-
-void SslServer::sslErrors(const QList<QSslError> &errors)
-{
-    foreach (const QSslError &error, errors)
-    {
-        CONSOLE_PRINT(error.errorString(), Console::eDEBUG);
-    }
 }
