6f62d50b1d2176e59b8b2e10aeda5985e046f8fe
Robosturm <commanderwars@gmx.de>
2022-08-18 16:38:52 +0200

    added missing building depot script

---
+++ resources/scripts/building/depot.js
@@ -0,0 +1,65 @@
+var Constructor = function()
+{
+    this.constructionList = ["INFANTRY", "MECH", "MOTORBIKE", "SNIPER", "RECON", "APC", "FLARE",
+                            "ARTILLERY", "LIGHT_TANK", "HOVERCRAFT", "FLAK", "ARTILLERYCRAFT", "HOVERFLAK",
+                            "ANTITANKCANNON", "HEAVY_TANK", "HEAVY_HOVERCRAFT", "NEOTANK",
+                            "MISSILE", "ROCKETTHROWER", "MEGATANK", "PIPERUNNER"];
+    this.getConstructionList = function(building)
+    {
+        return DEPOT.constructionList;
+    };
+    this.getRepairTypes = function(building)
+    {
+        return [GameEnums.UnitType_Ground, GameEnums.UnitType_Hovercraft, GameEnums.UnitType_Infantry];
+    };
+    this.startOfTurn = function(building, map)
+    {
+        BUILDING.replenishUnit(building, map, 0, 0.5, 0.5, 0.5, true);
+    };
+    this.loadSprites = function(building, neutral, map)
+    {
+        // neutral player
+        building.loadSprite("depot", false);
+        building.loadSprite("depot+shadow+" + BUILDING.getBuildingBaseTerrain(building, map), false);
+    };
+    this.getBaseIncome = function()
+    {
+        return 0;
+    };
+    this.getDefense = function()
+    {
+        return 2;
+    };
+    this.getMiniMapIcon = function()
+    {
+        return "minimap_depot";
+    };
+
+    this.getName = function()
+    {
+        return qsTr("Depot");
+    };
+
+    this.getDescription = function()
+    {
+        return qsTr("<r>A civilian cargo depot that provides adequate cover and </r><div c='#00ff00'>ground </div><r>units can </r><div c='#00ff00'>resupply</div><r> half their supplies each turn.</r>.");
+    };
+
+    this.getTerrainAnimationForeground = function(unit, terrain, defender, map)
+    {
+        return "";
+    };
+
+    this.getTerrainAnimationBackground = function(unit, terrain, defender, map)
+    {
+        var weatherModifier = TERRAIN.getWeatherModifier(map);
+        if (weatherModifier === "")
+        {
+            weatherModifier = TERRAIN.getTerrainWeatherModifier(terrain);
+        }
+        return "back_" + weatherModifier + "depot";
+    };
+}
+
+Constructor.prototype = BUILDING;
+var DEPOT = new Constructor();
