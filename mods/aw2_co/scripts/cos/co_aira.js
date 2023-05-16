3bdbbcad8e148cb75daaaaab267d60f92c9dc4c7
Robosturm <commanderwars@gmx.de>
2023-05-16 19:47:40 +0200

    added aira changes

--- mods/aw2_co/scripts/cos/co_aira.js
+++ mods/aw2_co/scripts/cos/co_aira.js
@@ -1,39 +1,6 @@
-CO_AIRA.getMovementFuelCostModifier = function(co, unit, fuelCost, map)
-{
-    if (co.getPowerMode() === GameEnums.PowerMode_Power &&
-            co.getOwner().isEnemyUnit(unit) === true)
-    {
-        return fuelCost * 5;
-    }
-    return 0;
-};
-CO_AIRA.coZoneBonus = 0;
-CO_AIRA.getMovementcostModifier = function(co, unit, posX, posY, map)
-{
-    if ((co.getPowerMode() === GameEnums.PowerMode_Superpower ||
-         co.getPowerMode() === GameEnums.PowerMode_Tagpower) &&
-            co.getOwner().isEnemyUnit(unit) === true)
-    {
-        return 1;
-    }
-    return 0;
-};
-
-CO_AIRA.getOffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                      defender, defPosX, defPosY, isDefender, action, luckmode, map)
-{
-    return 0;
-};
-
-CO_AIRA.getDeffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                       defender, defPosX, defPosY, isAttacker, action, luckmode, map)
-{
-    if (co.getIsCO0() === true)
-    {
-        if (co.getPowerMode() > GameEnums.PowerMode_Off)
-        {
-            return 10;
-        }
-    }
-    return 0;
-};
+this.superPowerDamage = 3;
+this.superPowerMovementCostIncrease = 1;
+this.powerFuelIncrease = 5;
+this.powerOffBonus = 0;
+this.powerDefBonus = 10;
+this.coZoneBonus = 0;
--- mods/awdc_co/scripts/cos/co_aira.js
+++ mods/awdc_co/scripts/cos/co_aira.js
@@ -6,6 +6,7 @@ CO_AIRA.init = function(co, map)
 CO_AIRA.activateSuperpower = function(co, powerMode, map)
 {
 	CO_AIRA.activatePower(co, map);
+    co.setPowerMode(GameEnums.PowerMode_Power);
 };
 CO_AIRA.getSuperPowerDescription = function()
 {
@@ -14,48 +15,4 @@ CO_AIRA.getSuperPowerDescription = function()
 CO_AIRA.getSuperPowerName = function()
 {
     return CO_AIRA.getPowerName();
-};
-CO_AIRA.getOffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                             defender, defPosX, defPosY, isDefender, action, luckmode, map)
-{
-    switch (co.getPowerMode())
-    {
-        case GameEnums.PowerMode_Tagpower:
-        case GameEnums.PowerMode_Superpower:
-        case GameEnums.PowerMode_Power:
-            return CO_AIRA.coZoneBonus;
-        default:
-            if (co.inCORange(Qt.point(atkPosX, atkPosY), attacker))
-            {
-                return CO_AIRA.coZoneBonus;
-            }
-            break;
-    }
-    return 0;
-};
-
-CO_AIRA.getMovementFuelCostModifier = function(co, unit, fuelCost, map)
-{
-    if (co.getPowerMode() > GameEnums.PowerMode_Off &&
-        co.getOwner().isEnemyUnit(unit) === true)
-    {
-        return fuelCost * 5;
-    }
-    return 0;
-};
-
-CO_AIRA.getDeffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                 defender, defPosX, defPosY, isAttacker, action, luckmode, map)
-    {
-        if (co.inCORange(Qt.point(defPosX, defPosY), defender) ||
-            co.getPowerMode() > GameEnums.PowerMode_Off)
-        {
-            return CO_AIRA.coZoneBonus;
-        }
-        return 0;
-    };
-
-CO_AIRA.getMovementcostModifier = function(co, unit, posX, posY, map)
-{
-    return 0;
 };
