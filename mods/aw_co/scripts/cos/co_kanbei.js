97ef7ad4fb5222e0c2aa74bbf0ff525d429418dd
Robosturm <commanderwars@gmx.de>
2023-05-18 21:26:35 +0200

    kanbei

--- mods/aw_co/scripts/cos/co_kanbei.js
+++ mods/aw_co/scripts/cos/co_kanbei.js
@@ -1,3 +1,15 @@
+CO_KANBEI.d2dcoZoneBonus = 0;
+
+CO_KANBEI.d2dBonus = 20;
+CO_KANBEI.d2dCostIncrease = 20;
+
+CO_KANBEI.powerOffBonus = 50;
+CO_KANBEI.powerDefBonus = 40;
+
+CO_KANBEI.superPowerOffBonus = 50;
+CO_KANBEI.superPowerCounterBonus = 100;
+CO_KANBEI.superPowerDefBonus = 60;
+
 CO_KANBEI.init = function(co, map)
 {
     co.setPowerStars(0);
@@ -6,6 +18,7 @@ CO_KANBEI.init = function(co, map)
 CO_KANBEI.activateSuperpower = function(co, powerMode, map)
 {
     CO_KANBEI.activatePower(co, map);
+    co.setPowerMode(GameEnums.PowerMode_Power);
 };
 CO_KANBEI.getSuperPowerDescription = function()
 {
@@ -15,53 +28,4 @@ CO_KANBEI.getSuperPowerName = function()
 {
     return CO_KANBEI.getPowerName();
 };
-CO_KANBEI.getCostModifier = function(co, id, baseCost, posX, posY, map)
-{
-    if (co.getIsCO0() === true)
-    {
-        return baseCost * CO_KANBEI.costIncrease / 100;
-    }
-    return 0;
-};
-CO_KANBEI.coZoneBonus = 0;
-CO_KANBEI.globalBonus = 20;
-CO_KANBEI.costIncrease = 20;
-CO_KANBEI.powerAtkBonus = 50;
-CO_KANBEI.superPowerAtkBonus = 50;
-CO_KANBEI.superPowerCounterBonus = 100;
-CO_KANBEI.powerDefBonus = 40;
-CO_KANBEI.superPowerDefBonus = 60;
-CO_KANBEI.getOffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                       defender, defPosX, defPosY, isDefender, action, luckmode, map)
-{
-    if (co.getIsCO0() === true)
-    {
-        switch (co.getPowerMode())
-        {
-        case GameEnums.PowerMode_Tagpower:
-        case GameEnums.PowerMode_Superpower:
-        case GameEnums.PowerMode_Power:
-            return CO_KANBEI.powerAtkBonus;
-        default:
-            return CO_KANBEI.globalBonus;
-        }
-    }
-    return 0;
-};
-CO_KANBEI.getDeffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                        defender, defPosX, defPosY, isAttacker, action, luckmode, map)
-{
-    if (co.getIsCO0() === true)
-    {
-        switch (co.getPowerMode())
-        {
-        case GameEnums.PowerMode_Tagpower:
-        case GameEnums.PowerMode_Superpower:
-        case GameEnums.PowerMode_Power:
-            return CO_KANBEI.powerDefBonus;
-        default:
-            return CO_KANBEI.globalBonus;
-        }
-    }
-    return 0;
-};
+
--- mods/awdc_co/scripts/cos/co_kanbei.js
+++ mods/awdc_co/scripts/cos/co_kanbei.js
@@ -6,6 +6,7 @@ CO_KANBEI.init = function(co, map)
 CO_KANBEI.activateSuperpower = function(co, powerMode, map)
 {
 	CO_KANBEI.activatePower(co, map);
+    co.setPowerMode(GameEnums.PowerMode_Power);
 };
 CO_KANBEI.getSuperPowerDescription = function()
 {
@@ -14,40 +15,4 @@ CO_KANBEI.getSuperPowerDescription = function()
 CO_KANBEI.getSuperPowerName = function()
 {
     return CO_KANBEI.getPowerName();
-};
-CO_KANBEI.getOffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                  defender, defPosX, defPosY, isDefender, action, luckmode, map)
-{
-    switch (co.getPowerMode())
-    {
-    case GameEnums.PowerMode_Tagpower:
-    case GameEnums.PowerMode_Superpower:
-    case GameEnums.PowerMode_Power:
-        return CO_KANBEI.powerAtkBonus;
-    default:
-        if (co.inCORange(Qt.point(atkPosX, atkPosY), attacker))
-        {
-            return CO_KANBEI.coZoneBonus;
-        }
-        break;
-    }
-    return CO_KANBEI.globalBonus;
-};
-CO_KANBEI.getDeffensiveBonus = function(co, attacker, atkPosX, atkPosY,
-                                   defender, defPosX, defPosY, isAttacker, action, luckmode, map)
-{
-    switch (co.getPowerMode())
-    {
-    case GameEnums.PowerMode_Tagpower:
-    case GameEnums.PowerMode_Superpower:
-    case GameEnums.PowerMode_Power:
-        return CO_KANBEI.powerDefBonus;
-    default:
-        if (co.inCORange(Qt.point(defPosX, defPosY), defender))
-        {
-            return CO_KANBEI.coZoneBonus;
-        }
-        break;
-    }
-    return CO_KANBEI.globalBonus;
 };
