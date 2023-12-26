8c7d25f35af0bc13a436a31db902b224892a0530
Robosturm <commanderwars@gmx.de>
2023-12-26 01:17:32 +0100

    test crash dump impl for linux / ios

--- coreengine/linux/crashreporter_os.cpp
+++ coreengine/linux/crashreporter_os.cpp
@@ -1,6 +1,18 @@
 #include "coreengine/crashreporter.h"
+#include <execinfo.h>
+#include <signal.h>
+
+void crashhandler(int sig)
+{
+    static qint32 constexpr stacksize = 30;
+    void *array[stacksize];
+    size_t size = backtrace(array, stacksize);
+    auto message = backtrace_symbols(array, size);
+    CrashReporter::_writeLog(QString("Error: signal ") + QString::number(sig) + " received");
+    CrashReporter::_writeLog(message);
+}
 
 void CrashReporter::setOsSignalHandler()
 {
-    // no implementation
+    signal(SIGSEGV, handler);
 }
