75e8d7a0b90ae4a3d5bba6db624454c7378ebe25
Robosturm <commanderwars@gmx.de>
2024-03-15 17:28:35 +0100

    #1776

--- mods/aw2_co/scripts/cos/co_olaf.js
+++ mods/aw2_co/scripts/cos/co_olaf.js
@@ -1,9 +1,9 @@
 CO_OLAF.superPowerDamage = 2;
-CO_OLAF.superPowerOffBonus = 60;
+CO_OLAF.superPowerOffBonus = 0;
 CO_OLAF.powerOffBonus = 10;
 CO_OLAF.powerDefBonus = 10;
-CO_OLAF.d2dOffBonus = 60;
+CO_OLAF.d2dOffBonus = 0;
 CO_OLAF.d2dRainMalus = true;
-CO_OLAF.d2dCoZoneOffBonus = 60;
+CO_OLAF.d2dCoZoneOffBonus = 0;
 CO_OLAF.d2dCoZoneBaseOffBonus = 10;
 CO_OLAF.d2dCoZoneDefBonus = 10;
