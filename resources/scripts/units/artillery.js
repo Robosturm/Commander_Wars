edb1b11b776bc2fe5f04672220c571622aac14af
FruityGumGuiltyFruit <142738275+FruityGumGuiltyFruit@users.noreply.github.com>
2024-04-24 20:57:53 -0400

    Update artillery.js

--- resources/scripts/units/artillery.js
+++ resources/scripts/units/artillery.js
@@ -47,7 +47,7 @@ var Constructor = function()
 
     this.getDescription = function()
     {
-        return qsTr("Indirect attack unit. Attack cannot be launched after unit has moved.");
+        return qsTr("Indirect-attack unit. Attack cannot be launched after unit has moved.");
     };
     this.getUnitType = function()
     {
