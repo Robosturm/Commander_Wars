35ab0b96cb11515ec1c6ba986b2c0e65f963f7ec
TheMasterCreed <whattheshmitty@gmail.com>
2026-08-29 22:02:11 -0500

    fixed hidden unit repair desync

--- resources/scripts/actions/ACTION_SUPPORTSINGLE_REPAIR.js
+++ resources/scripts/actions/ACTION_SUPPORTSINGLE_REPAIR.js
@@ -108,8 +108,8 @@ var Constructor = function()
             animation.addBox("info", map.getImageSize() / 2, 0, width + 36, map.getImageSize(), 400);
             animation.addSprite("repair", map.getImageSize() / 2 + 4, 4, 400, 2);
             animation.addSound("repair_2.wav");
-            repairUnit.refill(refillMaterial);
         }
+        repairUnit.refill(refillMaterial);
         UNIT.repairUnit(repairUnit, 1, map);
         ACTION_SUPPORTSINGLE_REPAIR.postAnimationUnit.setHasMoved(true);
         ACTION_SUPPORTSINGLE_REPAIR.postAnimationUnit = null;
